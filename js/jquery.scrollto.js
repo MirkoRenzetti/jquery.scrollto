b82faa30064542a80b924d6f69e22eb592100d67
Mirko Renzetti <mirko@Shinigami.(none)>
2014-06-14 12:20:42 +0200

    First commit

---
+++ js/jquery.scrollto.js
@@ -0,0 +1,21 @@
+/*!
+ * jQuery scrollTo plugin
+ * 
+ * Copyright Mirko Renzetti
+ */
+
+(function($) {
+	$.fn.scrollto = function(options) {
+
+		var defaults = {
+			'speed': 0.3
+		};
+		var custom = $.extend(defaults, options);
+		
+		return this.each(function() {
+			var $link = $(this);
+
+		});
+
+	};
+})(jQuery);
